29b2aa2e2dd2346ff9b968e5ed9b48a74bf52dcc
LUCIEN PIAT (Etudiant) <lucienpiat33@gmail.com>
2024-11-04 10:57:08 +0100

    remove home tab

--- functions.R
+++ functions.R
@@ -9,18 +9,6 @@
 # Function to create a custom sidebar menu item with an image
 #
 #@return a tabItem for the about section
-aboutTab <- function() {
-  tabItem(tabName = "about", 
-          h2("FEA-ther, Functional Enrichment Analysis"),
-          p("This tool was coded by Lucien Piat for the M2.1 BIMS program at Rouen Normandie University."),
-          p("The FEA-ther tool allows users to perform functional enrichment analysis on biological data."),
-          p("Users can visualize the results through interactive plots and tables."),
-          p("For more information or to contribute to the project, visit the GitHub repository:"),
-          a(href = "https://github.com/Lucien-Piat/FEAther", 
-            "https://github.com/Lucien-Piat/FEAther")
-  )
-}
-
 homeTab <- function() {
   tabItem(tabName = "home_mitem", 
           h2("FEA-ther, Functional Enrichment Analysis"),
@@ -45,5 +33,3 @@ show_shiny_error <- function(title, message) {
     html = TRUE
   )
 }
-
-
