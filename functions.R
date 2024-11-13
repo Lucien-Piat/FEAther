106640d2f7e51abdc41f70ad4114219807ac5552
LUCIEN PIAT (Etudiant) <lucienpiat33@gmail.com>
2024-11-13 09:58:14 +0100

    Update the about tab

--- functions.R
+++ functions.R
@@ -6,18 +6,63 @@
 # Description: Functions for the project
 # -----------------------------------------
 
+
+check_and_install_packages <- function(packages) {
+  # Identify packages that are not installed
+  new_packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+  
+  # Install missing packages
+  if (length(new_packages)) {
+    install.packages(new_packages)
+  }
+  
+  # Load all required packages
+  lapply(packages, require, character.only = TRUE)
+}
+
+
+create_dashboard_header <- function() {
+  dashboardHeader(
+    title = tags$div(style = "display: flex; align-items: center;",
+                     tags$img(src = "logo.png", height = "50px"),
+                     tags$span(style = "margin-left: 20px;", "FEA-ther")
+    ),
+    titleWidth = 230
+  )
+}
+
 # Function to create a custom sidebar menu item with an image
 #
 #@return a tabItem for the about section
-homeTab <- function() {
-  tabItem(tabName = "home_mitem", 
-          h2("FEA-ther, Functional Enrichment Analysis"),
-          p("This tool was coded by Lucien Piat for the M2.1 BIMS program at Rouen Normandie University."),
-          p("The FEA-ther tool allows users to perform functional enrichment analysis on biological data."),
-          p("Users can visualize the results through interactive plots and tables."),
-          p("For more information or to contribute to the project, visit the GitHub repository:"),
-          a(href = "https://github.com/Lucien-Piat/FEAther", 
-            "https://github.com/Lucien-Piat/FEAther")
+aboutTab <- function() {
+  tabItem(tabName = "about", 
+          h2("FEA-ther: Functional Enrichment Analysis Tool"),
+          
+          # Adding a brief description with bold keywords
+          p("Welcome to the ", strong("FEA-ther"), " tool. This application was developed by ", strong("Lucien Piat"), 
+            " as part of the M2.1 ", strong("Bioinformatics (BIMS) master"), 
+            " at ", strong("Rouen Normandie University"), "."),
+          
+          p("FEA-ther allows users to perform ", strong("functional enrichment analysis"), " on ", 
+            strong("biological data"), ". By using this tool, users can analyze and interpret biological data, ",
+            "identifying significantly enriched ", strong("GO terms"), " and ", strong("pathways"), " associated with their datasets."),
+          
+          # Adding more details about the tool's capabilities
+          p("The tool offers:", 
+            tags$ul(
+              tags$li("Interactive data upload with flexible file format support (", strong(".csv"), ", ", strong(".txt"), ", ", strong(".tsv"), ", ", strong(".dat"), ")."),
+              tags$li("Data inspection with customizable ", strong("volcano plots"), " and ", strong("filtering options"), " for fine-tuned analysis."),
+              tags$li("More to come..."),
+            )),
+          
+          # Including an image (e.g., logo or illustrative image for context)
+          tags$div(style = "text-align: center;",
+                   tags$img(src = "logo.png", height = "200px", alt = "FEA-ther logo")
+          ),
+          
+          # GitHub and contribution information
+          p("For more information or to contribute to the project, please visit the project's ", 
+            a(href = "https://github.com/Lucien-Piat/FEAther", "GitHub repository"), "."),
   )
 }
 
