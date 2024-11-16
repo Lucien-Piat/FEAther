79ff77d3906afa6dd443dfe163004fbd5123955b
Lpiat <lucienpiat33@gmail.com>
2024-11-16 19:59:09 +0100

    Unionize code formating

--- functions.R
+++ functions.R
@@ -12,66 +12,77 @@
 
 # Function to create a custom header with an image
 custom_dashboard_header <- function() {
-  dashboardHeader(
-    title = tags$div(
-      style = "display: flex; align-items: center;",
-      tags$img(src = "logo.png", height = "50px"),
-      tags$span(style = "margin-left: 20px;", "FEA-ther")
-    ),
-    titleWidth = 230
-  )
+    dashboardHeader(
+        title = tags$div(
+            style = "display: flex; align-items: center;",
+            tags$img(src = "logo.png", height = "50px"),
+            tags$span(style = "margin-left: 20px;", "FEA-ther")
+        ),
+        titleWidth = 230
+    )
 }
 
 # Function to create a custom unclickable home button with an image
 custom_home <- function() {
-  tags$li(
-    class = "nav-item",
-    tags$a(
-      href = "#",
-      class = "nav-link",
-      "Home, select your analysis ↓",
-      style = "font-size: 14px;",
-      tags$img(src = "item.png", height = "30px", style = "margin-left: 0px;")
+    tags$li(
+        class = "nav-item",
+        tags$a(
+            href = "#",
+            class = "nav-link",
+            "Home, select your analysis ↓",
+            style = "font-size: 14px;",
+            tags$img(src = "item.png", height = "30px", style = "margin-left: 0px;")
+        )
     )
-  )
 }
 
 # Function to create the about tab with useful infos
 aboutTab <- function() {
-  tabItem(
-    tabName = "about",
-    h2("FEA-ther: Functional Enrichment Analysis Tool"),
-    p("Welcome to the ", strong("FEA-ther"), " tool. This application was developed by ", strong("Lucien Piat"),
-      " as part of the M2.1 ", strong("Bioinformatics (BIMS) master"), " at ", strong("Rouen Normandie University"), "."),
-    p("FEA-ther allows users to perform ", strong("functional enrichment analysis"), " on ", strong("biological data"),
-      ". By using this tool, users can analyze and interpret biological data, identifying significantly enriched ",
-      strong("GO terms"), " and ", strong("pathways"), " associated with their datasets."),
-    p("The tool offers:", tags$ul(
-      tags$li("Interactive data upload with flexible file format support (", strong(".csv"), ", ", strong(".txt"),
-              ", ", strong(".tsv"), ", ", strong(".dat"), ")."),
-      tags$li("Data inspection with customizable ", strong("volcano plots"), " and ", strong("filtering options"),
-              " for fine-tuned analysis."),
-      tags$li("Output of the plot and table"),
-      tags$li("More to come..."))),
-    tags$div(
-      style = "text-align: center;",
-      tags$img(src = "logo.png", height = "200px", alt = "FEA-ther logo")
-    ),
-    p("For more information, please visit the project's ",
-      a(href = "https://github.com/Lucien-Piat/FEAther", "GitHub repository"), ".")
-  )
+    tabItem(
+        tabName = "about",
+        h2("FEA-ther: Functional Enrichment Analysis Tool"),
+        p(
+            "Welcome to the ", strong("FEA-ther"), " tool. This application was developed by ", strong("Lucien Piat"),
+            " as part of the M2.1 ", strong("Bioinformatics (BIMS) master"), " at ", strong("Rouen Normandie University"), "."
+        ),
+        p(
+            "FEA-ther allows users to perform ", strong("functional enrichment analysis"), " on ", strong("biological data"),
+            ". By using this tool, users can analyze and interpret biological data, identifying significantly enriched ",
+            strong("GO terms"), " and ", strong("pathways"), " associated with their datasets."
+        ),
+        p("The tool offers:", tags$ul(
+            tags$li(
+                "Interactive data upload with flexible file format support (", strong(".csv"), ", ", strong(".txt"),
+                ", ", strong(".tsv"), ", ", strong(".dat"), ")."
+            ),
+            tags$li(
+                "Data inspection with customizable ", strong("volcano plots"), " and ", strong("filtering options"),
+                " for fine-tuned analysis."
+            ),
+            tags$li("Output of the plot and table"),
+            tags$li("More to come...")
+        )),
+        tags$div(
+            style = "text-align: center;",
+            tags$img(src = "logo.png", height = "200px", alt = "FEA-ther logo")
+        ),
+        p(
+            "For more information, please visit the project's ",
+            a(href = "https://github.com/Lucien-Piat/FEAther", "GitHub repository"), "."
+        )
+    )
 }
 
 # Function to trigger shinyalert error with a custom message
 # @title Title of the popup
 # @message Printed message of the popup
 show_shiny_error <- function(title, message) {
-  shinyalert::shinyalert(
-    title = title,
-    text = tags$div(tags$img(src = "dodo.png", height = "50px"), tags$p(message)),
-    type = "error",
-    html = TRUE
-  )
+    shinyalert::shinyalert(
+        title = title,
+        text = tags$div(tags$img(src = "dodo.png", height = "50px"), tags$p(message)),
+        type = "error",
+        html = TRUE
+    )
 }
 
 # -----------------------------------------
@@ -84,19 +95,20 @@ show_shiny_error <- function(title, message) {
 # @pval_cutoff The threshold for p-value
 # @return A ggplot
 filtered_plot <- function(df, log2FC_cutoff, pval_cutoff) {
-  df$log10_pval <- -log10(df$pval)
-  df$color <- ifelse(df$pval >= pval_cutoff, "grey",
-                     ifelse(df$log2FC < -log2FC_cutoff, "red", ifelse(df$log2FC > log2FC_cutoff, "green", "grey")))
-  plot <- ggplot(df, aes(
-    x = log2FC,
-    y = log10_pval,
-    tooltip = paste("Gene:", GeneName, "<br>ID:", ID, "<br>log2FC:", log2FC, "<br>-log10(pval):", log10_pval)
-  )) +
-    geom_point_interactive(aes(color = color), size = 1) +
-    labs(x = "log2 Fold Change (log2FC)", y = "-log10(p-value)") +
-    ylim(0, 10) +
-    scale_color_identity() +
-    theme_minimal() +
-    theme(legend.position = "none")
-  return(plot)
+    df$log10_pval <- -log10(df$pval)
+    df$color <- ifelse(df$pval >= pval_cutoff, "grey",
+        ifelse(df$log2FC < -log2FC_cutoff, "red", ifelse(df$log2FC > log2FC_cutoff, "green", "grey"))
+    )
+    plot <- ggplot(df, aes(
+        x = log2FC,
+        y = log10_pval,
+        tooltip = paste("Gene:", GeneName, "<br>ID:", ID, "<br>log2FC:", log2FC, "<br>-log10(pval):", log10_pval)
+    )) +
+        geom_point_interactive(aes(color = color), size = 1) +
+        labs(x = "log2 Fold Change (log2FC)", y = "-log10(p-value)") +
+        ylim(0, 10) +
+        scale_color_identity() +
+        theme_minimal() +
+        theme(legend.position = "none")
+    return(plot)
 }
--- server.R
+++ server.R
@@ -19,90 +19,104 @@ source("functions.R")
 
 # Server side of the app
 server <- function(input, output, session) {
-  # -----------------------------------------
-  # Data input
-  # -----------------------------------------
-  
-  # Reactive expression for loading and processing the data
-  data <- reactive({
-    req(input$file) # Ensure file input is available
-    
-    # Check if the uploaded file has a valid extension (CSV, TXT, TSV, or DAT)
-    file_ext <- tools::file_ext(input$file$name)
-    if (!(file_ext %in% c("csv", "txt", "tsv", "dat"))) {
-      show_shiny_error("File upload error", "Please upload a tabular data file with one of the following extensions: .csv, .txt, .tsv, or .dat.")
-      return(NULL)
-    }
-    
-    # Add the separator for fread
-    separator <- switch(input$separator, Auto = "auto", Semicolon = ";", Comma = ",", Tab = "\t", Space = " ", Dot = ".")
-    
-    # Define the desired column names
-    column_names <- c("GeneName", "ID", "baseMean", "log2FC", "pval", "padj")
-    
-    # Use fread with specified column names
-    df <- tryCatch({
-      fread(input$file$datapath, sep = separator, header = TRUE, col.names = column_names)
-    }, error = function(e) {
-      show_shiny_error("File upload error", HTML("There was an error reading the file.<br><br>Provide a file with exactly 6 columns:<br>'GeneName', 'ID', 'baseMean', 'log2FC', 'pval', and 'padj'.<br><br>A header row is optional."))
-      return(NULL)
+    # -----------------------------------------
+    # Data input
+    # -----------------------------------------
+
+    # Reactive expression for loading and processing the data
+    data <- reactive({
+        req(input$file) # Ensure file input is available
+
+        # Check if the uploaded file has a valid extension (CSV, TXT, TSV, or DAT)
+        file_ext <- tools::file_ext(input$file$name)
+        if (!(file_ext %in% c("csv", "txt", "tsv", "dat"))) {
+            show_shiny_error("File upload error", "Please upload a tabular data file with one of the following extensions: .csv, .txt, .tsv, or .dat.")
+            return(NULL)
+        }
+
+        # Add the separator for fread
+        separator <- switch(input$separator,
+            Auto = "auto",
+            Semicolon = ";",
+            Comma = ",",
+            Tab = "\t",
+            Space = " ",
+            Dot = "."
+        )
+
+        # Define the desired column names
+        column_names <- c("GeneName", "ID", "baseMean", "log2FC", "pval", "padj")
+
+        # Use fread with specified column names
+        df <- tryCatch(
+            {
+                fread(input$file$datapath, sep = separator, header = TRUE, col.names = column_names)
+            },
+            error = function(e) {
+                show_shiny_error("File upload error", HTML("There was an error reading the file.<br><br>Provide a file with exactly 6 columns:<br>'GeneName', 'ID', 'baseMean', 'log2FC', 'pval', and 'padj'.<br><br>A header row is optional."))
+                return(NULL)
+            }
+        )
+        req(nrow(df) > 0) # Ensure the data frame is not empty
+        df # Return the dataframe
     })
-    req(nrow(df) > 0) # Ensure the data frame is not empty
-    df # Return the dataframe
-  })
-  
-  # -----------------------------------------
-  # Filter data
-  # -----------------------------------------
-  
-  # Reactive expression for filtering the data based on slider inputs
-  filtered_data <- reactive({
-    df <- req(data()) # Ensure data is available
-    df_filtered <- df %>% dplyr::filter(abs(log2FC) >= input$log2FC_slider, pval <= input$p_val_slider)
-    df_filtered # Return the filtered dataframe
-  })
-  
-  # -----------------------------------------
-  # Create the volcano plot
-  # -----------------------------------------
-  
-  volcano_plot <- reactive({
-    df <- req(data()) # Ensure data is available
-    plot <- filtered_plot(df, input$log2FC_slider, input$p_val_slider) # Create a volcano plot
-    girafe(ggobj = plot, options = list(opts_hover(css = "fill:#FF6347;stroke:black;cursor:pointer;"), opts_zoom(min = 1, max = 5))) # Interactive plot
-  })
-  
-  # Render the volcano plot
-  output$volcano_plot <- renderGirafe({ volcano_plot() })
-  
-  # -----------------------------------------
-  # Download
-  # -----------------------------------------
-  
-  output$download <- downloadHandler(
-    filename = function() {
-      if (input$downloadType == "plot") paste("volcano_plot_", Sys.Date(), ".png", sep = "")
-      else paste("filtered_data_", Sys.Date(), ".csv", sep = "")
-    },
-    content = function(file) {
-      if (input$downloadType == "plot") {
+
+    # -----------------------------------------
+    # Filter data
+    # -----------------------------------------
+
+    # Reactive expression for filtering the data based on slider inputs
+    filtered_data <- reactive({
         df <- req(data()) # Ensure data is available
-        plot <- filtered_plot(df, input$log2FC_slider, input$p_val_slider)
-        ggsave(file, plot = plot, device = "png", width = 8, height = 6, dpi = 300)
-      } else if (input$downloadType == "table") {
-        write.csv(filtered_data(), file, row.names = FALSE) # Download CSV
-      }
-    }
-  )
-  
-  # -----------------------------------------
-  # Render the Table
-  # -----------------------------------------
-  
-  output$table <- DT::renderDataTable({
-    data <- filtered_data()
-    req(data)  # Ensure data is available
-    datatable(data)  # Render table without additional options
-  })
-  
+        df_filtered <- df %>% dplyr::filter(abs(log2FC) >= input$log2FC_slider, pval <= input$p_val_slider)
+        df_filtered # Return the filtered dataframe
+    })
+
+    # -----------------------------------------
+    # Create the volcano plot
+    # -----------------------------------------
+
+    volcano_plot <- reactive({
+        df <- req(data()) # Ensure data is available
+        plot <- filtered_plot(df, input$log2FC_slider, input$p_val_slider) # Create a volcano plot
+        girafe(ggobj = plot, options = list(opts_hover(css = "fill:#FF6347;stroke:black;cursor:pointer;"), opts_zoom(min = 1, max = 5))) # Interactive plot
+    })
+
+    # Render the volcano plot
+    output$volcano_plot <- renderGirafe({
+        volcano_plot()
+    })
+
+    # -----------------------------------------
+    # Download
+    # -----------------------------------------
+
+    output$download <- downloadHandler(
+        filename = function() {
+            if (input$downloadType == "plot") {
+                paste("volcano_plot_", Sys.Date(), ".png", sep = "")
+            } else {
+                paste("filtered_data_", Sys.Date(), ".csv", sep = "")
+            }
+        },
+        content = function(file) {
+            if (input$downloadType == "plot") {
+                df <- req(data()) # Ensure data is available
+                plot <- filtered_plot(df, input$log2FC_slider, input$p_val_slider)
+                ggsave(file, plot = plot, device = "png", width = 8, height = 6, dpi = 300)
+            } else if (input$downloadType == "table") {
+                write.csv(filtered_data(), file, row.names = FALSE) # Download CSV
+            }
+        }
+    )
+
+    # -----------------------------------------
+    # Render the Table
+    # -----------------------------------------
+
+    output$table <- DT::renderDataTable({
+        data <- filtered_data()
+        req(data) # Ensure data is available
+        datatable(data) # Render table without additional options
+    })
 }
